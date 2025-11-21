834bcafe083e16bc55505e60f0cfd930767cd499
Pedro Ruthes <pedroo.ruthes@gmail.com>
2025-11-21 20:16:57 -0300

    feat: Input component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,15 @@
 import { View, Image } from 'react-native'
 import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+
 import { styles } from './styles'
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
-      <Button title="Entrar"/>
+      <Input placeholder="O que você precisa comprar?" />
+      <Button title="Adicionar"/>
     </View>
   )
 }
