10f31eae234d39230b7897b5aca7bcc846bd411c
Pedro Ruthes <pedroo.ruthes@gmail.com>
2025-11-21 19:56:49 -0300

    first commit

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,12 @@
+import { View, Image } from 'react-native'
+import { Button } from '@/components/Button'
+import { styles } from './styles'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
+      <Button />
+    </View>
+  )
+}
---
+++ src/app/Home/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      backgroundColor: "#D0D2D8",
+      paddingTop: 62,
+      padding: 24
+    },
+    logo: {
+      height: 34,
+      width: 134
+    }
+});
