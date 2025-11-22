6d382b12e20db4f6af9c1a8c580d9b9abe806ecc
Pedro Ruthes <pedroo.ruthes@gmail.com>
2025-11-22 20:57:53 -0300

    feat: Filter and StatusIcon components

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,6 +1,8 @@
 import { View, Image } from "react-native";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
 
 import { styles } from "./styles";
 
@@ -14,7 +16,10 @@ export default function Home() {
         <Button title="Adicionar" />
       </View>
 
-      <View style={styles.content}></View>
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
