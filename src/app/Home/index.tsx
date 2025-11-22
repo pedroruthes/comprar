211aa53524c6e6a4a44beaaf417d26faceb9c998
Pedro Ruthes <pedroo.ruthes@gmail.com>
2025-11-22 20:38:09 -0300

    feat: Content style

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,15 +1,20 @@
-import { View, Image } from 'react-native'
-import { Button } from '@/components/Button'
-import { Input } from '@/components/Input'
+import { View, Image } from "react-native";
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
-import { styles } from './styles'
+import { styles } from "./styles";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Image source={require('@/assets/logo.png')} style={styles.logo} />
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title="Adicionar"/>
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
-  )
+  );
 }
